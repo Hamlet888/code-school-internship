f26ba1b5deaf94ebdaeab58adbba4c7a32a245f0
artosargsyan10 <artosargsyan10@gmail.com>
2019-07-02 22:43:06 +0400

    Leeson 2 02/07/2019

--- src/main/java/code/school/internship/Lesson_2_July_02.java
+++ src/main/java/code/school/internship/Lesson_2_July_02.java
@@ -1,6 +1,16 @@
 package code.school.internship;
 
 public class Lesson_2_July_02 {
+    public static void main(String[] args) {
+        double pi,r,a;
+        r = 10.8; // radius of cirle
+        pi = 3.1416; // pi,approximently
+        a = pi * r * r; // compute area
+        System.out.println("Area of circle is" + a);
+    }
 
 
 }
+
+
+
